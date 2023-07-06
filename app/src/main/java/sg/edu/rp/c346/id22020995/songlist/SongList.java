a596781e565d64b542d17651b5451ea9257f5474
Shannon <22020995@myrp.edu.sg>
2023-07-06 16:04:10 +0800

    SongList.java updated

--- app/src/main/java/sg/edu/rp/c346/id22020995/songlist/SongList.java
+++ app/src/main/java/sg/edu/rp/c346/id22020995/songlist/SongList.java
@@ -3,6 +3,8 @@ package sg.edu.rp.c346.id22020995.songlist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -35,7 +37,5 @@ public class SongList extends AppCompatActivity {
         ArrayList<Song> songList = db.getSongs();
         ArrayAdapter aaSongs = new ArrayAdapter<>(SongList.this, android.R.layout.simple_list_item_1, songList);
         lvSongs.setAdapter(aaSongs);
-
-        registerForContextMenu(lvSongs);
     }
 }
